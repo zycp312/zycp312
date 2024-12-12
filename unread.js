017fcda3e0e578385a2c8ce7ee4fd2b6b1021790
zycp312 <zycp312@gmail.com>
2024-12-12 22:07:03 +0400

    .

--- unread.js
+++ unread.js
@@ -1,23 +1,24 @@
+
 class Unread {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.hovering = false;
-      this.shape = function () {
-        push();
-        fill(246, 187, 0);
-        textSize(70);
-        text("!", this.x + 50, this.y + 72);
-        pop();
-      };
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.hovering = false;
+    this.shape = function () {
+      push();
+      fill(246, 187, 0);
+      textSize(70);
+      text("!", this.x + 50, this.y + 72);
+      pop();
+    };
+  }
+  display() {
+    if (collidePointRect(mouseX, mouseY, this.x, this.y, 100, 100)) {
+      this.hovering = true;
     }
-    display() {
-      if (collidePointRect(mouseX, mouseY, this.x, this.y, 100, 100)) {
-        this.hovering = true;
-      }
-      if (this.hovering == false) {
-        // circle(this.x + 50, this.y + 50, 35);
-        this.shape();
-      }
+    if (this.hovering == false) {
+      // circle(this.x + 50, this.y + 50, 35);
+      this.shape();
     }
   }
+}
